45fc4ce73b08e0832ed3f7a6259bc9a6a039985e
Helvijs <helvijs.adams@gmail.com>
2018-04-25 12:53:50 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,80 @@
+// MIT License
+// 
+// Copyright (c) 2018 Helvijs Adams
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+//	Set clock frequency
+#ifndef F_CPU
+#define F_CPU 16000000UL
+#endif
+
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+
+//	Set up UART for printf();
+#ifndef BAUD
+#define BAUD 9600
+#endif
+#include "STDIO_UART.h"
+
+//	Include nRF24L01+ library
+#include "nrf24l01.h"
+
+//	Used in IRQ ISR
+volatile bool message_received = false;
+
+int main(void)
+{	
+	//	Set cliche message to send (message cannot exceed 32 characters)
+	char tx_message[32];				// Define string array
+	strcpy(tx_message,"Hello World!");	// Copy string into array
+	
+	//	Initialize UART
+	uart_init();
+	
+	//	Initialize nRF24L01+ and print configuration info
+    nrf24_init();
+	print_config();
+	
+	//	Start listening to incoming messages
+	nrf24_start_listening();
+	
+    while (1) 
+    {
+		if (message_received)
+		{
+			//	Message received, print it
+			message_received = false;
+			printf("Received message: %s\n",nrf24_read_message());
+			//	Send message as response
+			nrf24_send_message(tx_message);
+		}	
+    }
+}
+
+//	Interrupt on IRQ pin
+ISR(INT0_vect) 
+{
+	message_received = true;
+}
